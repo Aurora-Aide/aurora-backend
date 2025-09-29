f41d0e759b8f66e7f39e9dd1e74560f4b1703f5d
kalinaterziyska <116567570+kalinaterziyska@users.noreply.github.com>
2025-09-29 19:36:01 +0300

    initial jwt authentication
    0.1

--- aurora_backend/aurora_backend/urls.py
+++ aurora_backend/aurora_backend/urls.py
@@ -15,12 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import path
-from authentication import views
+from django.urls import path, include
+# from .views import (
+
+# )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name="home"),
+    # path('admin/', admin.site.urls),
+    path('authentication/', include('authentication.urls')),
 ]
 
